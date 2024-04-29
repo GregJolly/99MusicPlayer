22c284fe4db0e368d7e9dfe49f7a648e214485c9
GregJolly <gregjofficial@gmail.com>
2024-04-29 14:02:54 -0500

    Added first draft of music player

---
+++ app.js
@@ -0,0 +1,119 @@
+let progress = document.getElementById("progress");
+let audio = document.querySelector("#song");
+let ctrlIcon= document.querySelector("#ctrlIcon");
+let songName = document.querySelector(".song-name");
+let artistName = document.querySelector(".artist-name");
+let coverArt = document.querySelector(".song-img"); 
+const forwardBtn = document.querySelector("#forward");
+const backBtn = document.querySelector("#back");
+let albumName = document.querySelector(".album-name");
+let download = document.querySelector("#download");
+let downloadLink = document.querySelector("#downloadLink");
+let spotifyBtn = document.querySelector("#spotify-btn");
+let spotifyLink =  document.querySelector("#spotify-link");
+let currentMusic = 0; 
+
+
+ctrlIcon.addEventListener("click", ()=>
+{
+    if (ctrlIcon.classList.contains("fa-pause"))
+    {
+        audio.pause();
+        ctrlIcon.classList.remove("fa-pause"); 
+        ctrlIcon.classList.add("fa-play");
+    }
+    else
+    {
+        audio.play();  
+        ctrlIcon.classList.add("fa-pause");             
+        ctrlIcon.classList.remove("fa-play");
+    }
+
+});
+ 
+const setMusic = (i) => 
+{
+    progress.value = 0;
+    let song = songs[i];
+    currentMusic = i;
+    audio.src = song.path;
+
+    songName.innerHTML = song.name; 
+    artistName.innerHTML = song.artist; 
+    coverArt.src = song.imgSrc; 
+    albumName.innerHTML = song.album;
+    
+    setTimeout(()=>{
+
+        progress.max = audio.duration; 
+       
+    }, 300);
+
+}
+
+setMusic(0); 
+
+setInterval(()=>{
+    
+    progress.value = audio.currentTime;  
+}, 500);
+
+
+progress.addEventListener("click",()=>{
+    audio.currentTime = progress.value;
+  
+});
+
+
+forwardBtn.addEventListener("click", ()=>
+{
+    if (currentMusic >= songs.length - 1)
+    {
+        currentMusic = 0;
+    }
+    else 
+    {
+        currentMusic ++; 
+    }
+
+    setMusic(currentMusic); 
+    audio.play();  
+    ctrlIcon.classList.add("fa-pause");             
+    ctrlIcon.classList.remove("fa-play");
+ 
+})
+backBtn.addEventListener("click", ()=>
+{
+    if (currentMusic <= 0)
+    {
+        currentMusic = song.length-1 ;
+    }
+    else    
+    {
+        currentMusic--; 
+    }
+
+    setMusic(currentMusic); 
+    audio.play();  
+    ctrlIcon.classList.add("fa-pause");             
+    ctrlIcon.classList.remove("fa-play");
+
+})
+
+
+download.addEventListener('click', () => {
+    if (audio.src) {
+        const fileName = audio.src.split('/').pop(); // Extract filename from audio URL
+        downloadLink.href = audio.src;
+        downloadLink.download = fileName;
+        downloadLink.click();
+    } else {
+        console.error('Audio source not found.');
+    }
+});
+
+spotifyBtn.addEventListener('click', ()=>
+{   
+    const currentSong = songs[currentMusic];
+    spotifyLink.href = currentSong.spotifyLnk;
+});
---
+++ data.js
@@ -0,0 +1,75 @@
+const songs = [
+    {
+
+        name: "WONDERLAND",
+        path: "media/WONDERLAND.mp3",
+        artist: "99wolv", 
+        imgSrc: "media/ephemera.png",
+        album: "EPHEMERA",
+        spotifyLnk: "https://open.spotify.com/track/3KpJX06HmGw6RwByv6u7Hx?si=e6876030da5340b6"
+    },
+    {
+
+        name: "STAY",
+        path: "media/STAY.mp3",
+        artist: "99wolv", 
+        imgSrc: "media/ephemera.png",
+        album: "EPHEMERA",
+        spotifyLnk: "https://open.spotify.com/track/1N9N4cYBNr92EfB8uUFdti?si=00974eba3dfc4db4"
+    },
+    {
+
+        name: "DRAFTS",
+        path: "media/DRAFTS.mp3",
+        artist: "99wolv", 
+        imgSrc: "media/ephemera.png",
+        album: "EPHEMERA",
+        spotifyLnk: "https://open.spotify.com/track/2BjzNkhkuIV54w5RtH80Hs?si=60e005a0a2eb410f"
+    },
+    {
+
+        name: "MICHELLE",
+        path: "media/MICHELLE.mp3",
+        artist: "99wolv", 
+        imgSrc: "media/ephemera.png",
+        album: "EPHEMERA",
+        spotifyLnk: "https://open.spotify.com/track/34UVLYn9tFZMBTAa1q5W4j?si=c926b0cadf184aeb"
+    },
+    {
+
+        name: "ANGELINA",
+        path: "media/ANGELINA.mp3",
+        artist: "99wolv", 
+        imgSrc: "media/ephemera.png",
+        album: "EPHEMERA",
+        spotifyLnk: "https://open.spotify.com/track/5M8NTCDfqbrNwJXNoCJvoa?si=41ea10b190494921"
+    },
+    {
+
+        name: "FEVER",
+        path: "media/FEVER.mp3",
+        artist: "99wolv", 
+        imgSrc: "media/ephemera.png",
+        album: "EPHEMERA",
+        spotifyLnk: "https://open.spotify.com/track/4bEGnicawvGdeaxDtuyjLp?si=64b8cf1baffb4df6"
+    
+    },
+    {
+
+        name: "418",
+        path: "media/418.mp3",
+        artist: "99wolv", 
+        imgSrc: "media/ephemera.png",
+        album: "EPHEMERA",
+        spotifyLnk: "https://open.spotify.com/track/03XLzRYWS9XSFlRonZtBqg?si=cb10dd66bfb74420"
+    },
+    {
+
+        name: "LOVE AGAIN",
+        path: "media/LOVEAGAIN.mp3",
+        artist: "99wolv", 
+        imgSrc: "media/ephemera.png",
+        album: "EPHEMERA",
+        spotifyLnk: "https://open.spotify.com/track/3sHQYFLvLKast3x5Anr9A8?si=b9f40a42b94744c5"
+    },
+]
